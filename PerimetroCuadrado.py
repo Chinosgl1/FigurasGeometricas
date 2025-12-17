14df804e0f2626e11817bf2564b1405b288a21c1
Chinosgl1 <emmanuelgarcialibrado7@gmail.com>
2025-12-17 08:25:19 -0600

    hice mi parte santiago (perimetro cuadrado)

---
+++ PerimetroCuadrado.py
@@ -0,0 +1,7 @@
+# Programa creado por Santiago Méndez García (17/12/2025)
+print("Programa creado por Santiago Méndez García")
+def PerimetroCuadrado():
+    lado  = float(input("Ingresa uno de los lados del cuadrado: "))
+    return lado * 4
+resultado = PerimetroCuadrado()
+print(f"Según los calculos (Y mis bastos conocimientos en ChatGPT) el resultado es: {resultado}")
