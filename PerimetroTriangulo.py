d48a1b80264ed0171d9f26ae57b438f16bee0c43
Chinosgl1 <emmanuelgarcialibrado7@gmail.com>
2025-12-17 08:20:45 -0600

    hice mi parte emmanuel (perimetro triangulo)

---
+++ PerimetroTriangulo.py
@@ -0,0 +1,11 @@
+print("ESTE FUE CREADO POR EMMANUEL")
+
+lado1 = float(input("Ingrese la longitud del primer lado: "))
+lado2 = float(input("Ingrese la longitud del segundo lado: "))
+lado3 = float(input("Ingrese la longitud del tercer lado: "))
+
+
+perimetro = lado1 + lado2 + lado3
+
+
+print("El perímetro del triángulo es:", perimetro)
